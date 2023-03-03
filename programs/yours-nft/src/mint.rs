622fd515e3b23975c673384f747300fe2a5bf6b9
xi-one <ssly1133@naver.com>
2023-03-03 18:48:41 +0900

    add mint code

---
+++ programs/yours-nft/src/mint.rs
@@ -0,0 +1,181 @@
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+use {
+    anchor_lang::{prelude::*, solana_program::program::invoke, system_program},
+    anchor_spl::{associated_token, token},
+    mpl_token_metadata::{instruction as token_instruction, ID as TOKEN_METADATA_ID},
+};
+
+pub fn mint(
+    ctx: Context<MintNFT>,
+    metadata_title: String,
+    metadata_symbol: String,
+    metadata_uri: String,
+) -> Result<()> {
+    create_mint_account(&ctx)?;
+    initialize_mint_account(&ctx)?;
+    create_token_account(&ctx)?;
+    mint_token_to_account(&ctx)?;
+    create_metadata_account(&ctx, metadata_title, metadata_symbol, metadata_uri)?;
+    create_master_edition_metadata_account(&ctx)?;
+    msg!("Token mint process completed successfully.");
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct MintNFT<'info> {
+    /// CHECK: We are about to create this with Metaplex
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: We are about to create this with Metaplex
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub mint: Signer<'info>,
+    /// CHECK: We are about to create this with Anchor
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    /// CHECK: Metaplex will check this
+    pub token_metadata_program: UncheckedAccount<'info>,
+}
+
+fn create_mint_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Creating mint account...");
+    msg!("Mint: {}", &ctx.accounts.mint.key());
+    system_program::create_account(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            system_program::CreateAccount {
+                from: ctx.accounts.mint_authority.to_account_info(),
+                to: ctx.accounts.mint.to_account_info(),
+            },
+        ),
+        10000000,
+        82,
+        &ctx.accounts.token_program.key(),
+    )
+}
+
+fn initialize_mint_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Initializing mint account...");
+    msg!("Mint: {}", &ctx.accounts.mint.key());
+    token::initialize_mint(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::InitializeMint {
+                mint: ctx.accounts.mint.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+        ),
+        0,
+        &ctx.accounts.mint_authority.key(),
+        Some(&ctx.accounts.mint_authority.key()),
+    )
+}
+
+fn create_token_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Creating token account...");
+    msg!("Token Address: {}", &ctx.accounts.token_account.key());
+    associated_token::create(CpiContext::new(
+        ctx.accounts.associated_token_program.to_account_info(),
+        associated_token::Create {
+            payer: ctx.accounts.mint_authority.to_account_info(),
+            associated_token: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.mint_authority.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            token_program: ctx.accounts.token_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+        },
+    ))
+}
+
+fn mint_token_to_account(ctx: &Context<MintNFT>) -> Result<()> {
+    msg!("Minting token to token account...");
+    msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key());
+    msg!("Token Address: {}", &ctx.accounts.token_account.key());
+    token::mint_to(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
+            },
+        ),
+        1,
+    )
+}
+
+fn create_metadata_account(
+    ctx: &Context<MintNFT>,
+    metadata_title: String,
+    metadata_symbol: String,
+    metadata_uri: String,
+) -> ProgramResult {
+    msg!("Creating metadata account...");
+    msg!(
+        "Metadata account address: {}",
+        &ctx.accounts.metadata.to_account_info().key()
+    );
+    invoke(
+        &token_instruction::create_metadata_accounts_v2(
+            TOKEN_METADATA_ID,
+            ctx.accounts.metadata.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.mint_authority.key(),
+            metadata_title,
+            metadata_symbol,
+            metadata_uri,
+            None,
+            1,
+            true,
+            false,
+            None,
+            None,
+        ),
+        &[
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_account.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+    )
+}
+
+fn create_master_edition_metadata_account(ctx: &Context<MintNFT>) -> ProgramResult {
+    msg!("Creating master edition metadata account...");
+    msg!(
+        "Master edition metadata account address: {}",
+        &ctx.accounts.master_edition.to_account_info().key()
+    );
+    invoke(
+        &token_instruction::create_master_edition_v3(
+            TOKEN_METADATA_ID,
+            ctx.accounts.master_edition.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.metadata.key(),
+            ctx.accounts.mint_authority.key(),
+            Some(0),
+        ),
+        &[
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_account.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+    )
+}
